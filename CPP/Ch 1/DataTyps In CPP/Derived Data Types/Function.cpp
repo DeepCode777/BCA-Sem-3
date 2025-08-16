015996dd636f328034292ca40ddc52a4c6450471
Code With Deep <deeppoptani777@gmail.com>
2025-08-16 18:50:04 +0530

    Create DataTyps In CPP/Derived Data Types/Function.cpp

---
+++ CPP/Ch 1/DataTyps In CPP/Derived Data Types/Function.cpp
@@ -0,0 +1,13 @@
+// #17 Example - Function In CPP
+
+#include <iostream>
+using namespace std;
+
+void myout(){
+    cout << " I Just Called " << endl;
+}
+
+int main() {
+    myout (); // Calling Function Here
+    return 0;
+}
