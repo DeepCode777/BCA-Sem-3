55259758d4280e69ccff451e907b6f4f12bf9815
Code With Deep <deeppoptani777@gmail.com>
2025-08-16 19:21:54 +0530

    Create Array.cpp

---
+++ CPP/Ch 1/DataTyps In CPP/Derived Data Types/Array.cpp
@@ -0,0 +1,13 @@
+// #18 Exampl Of Array In CPP
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    int arr[5] = {10,20,30,40,50};
+    for(i = 0; i < 5; i++){
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
