db3c6c51477ffbea08762d107f1a94be4aa10477
Code With Deep <deeppoptani777@gmail.com>
2025-08-17 09:31:01 +0530

    Create DataType Conversion/1_Implisit_Type_Casting.cpp

---
+++ CPP/Ch 1/DataType Conversion/1_Implisit_Type_Casting.cpp
@@ -0,0 +1,13 @@
+// #25 Example - Implisit Type Casting/Conversion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    short x = 20 ;
+    int y = x ; // (Small (x) to Large(y))
+
+    cout << "The value Of X = " << x << endl ; // 20
+    cout << " The Values of Y = " << y <<endl;  //20
+    return 0;
+}
