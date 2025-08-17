cd25716b8c567c298f8d84a03fc74e118ceb1f56
Code With Deep <deeppoptani777@gmail.com>
2025-08-17 09:34:22 +0530

    Create 2_Explisit_Type_Casting.cpp

---
+++ CPP/Ch 1/DataType Conversion/2_Explisit_Type_Casting.cpp
@@ -0,0 +1,13 @@
+// #26 Example - Explit Type Casting/ Conversion
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double x = 20.3;
+    int y = (int)x; // double To int (Large To Small)
+    
+    cout <<"Value X = "<< x << endl;
+    cout << "Value Y = "<< y << endl;
+    return 0;
+}
