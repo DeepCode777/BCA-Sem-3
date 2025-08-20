3c50f0c06cc18efdffb1477fbfde862dc5f34a80
Code With Deep <deeppoptani777@gmail.com>
2025-08-20 13:03:11 +0530

    Create Friend_Function.cpp

---
+++ CPP/Chapter 2/Friend_Function.cpp
@@ -0,0 +1,45 @@
+// #17-18 Example Friend Function
+
+#include <iostream>
+using namespace std;
+
+class empl{ //class one
+    int salary = 4000;
+    friend void sal(empl e); //Defind Friend Function
+};
+void sal(empl e){
+    cout << "\n Salary : " << e.salary << endl;
+}
+
+
+class largest{ // class two
+    int a,b;
+    public :
+    void data(){
+        cout << "\n Enter A : ";
+        cin >> a;
+        cout << "\n Enter B : ";
+        cin >> b;
+        
+        cout << endl << " Value Of A : "<< a << endl << " Value Of B ; "<< b;
+    }
+    friend void max(largest l); //Defind Friend Function
+};
+void max(largest l){
+    if (l.a > l.b){
+        cout << "\n A is Greater ";
+    }else{
+        cout << "\n B Is Greater ";
+    }
+}
+int main() {
+    //class one here
+    empl e;
+    sal(e); 
+    
+    //class two here
+    largest l1;
+    l1.data();
+    max(l1);
+    return 0;
+}
