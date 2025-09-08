108a70df1a01e26efafcd8ecd104ec64d36fcf7c
Code With Deep <deeppoptani777@gmail.com>
2025-09-08 19:04:52 +0530

    Create Practical_1.sql
    It's Just My First Oracle Table...

---
+++ Oracle/Practical Assignments/Practical_1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE heros(no NUMBER (5) PRIMARY KEY, name VARCHAR2 (30), city VARCHAR2 (20));
+
+INSERT INTO heros VALUES(1,'Lucifer Morningstar','Los Angeles');
+INSERT INTO heros VALUES(2,'Tony Stark','New York');
+INSERT INTO heros VALUES(3,'Peter Parker','New York');
+INSERT INTO heros VALUES(4,'Clint Barton','America');
+
+SELECT * FROM heros;
