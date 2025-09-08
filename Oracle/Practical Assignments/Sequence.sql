a15be1648375eeeefd585c766a9c514a9c4035f8
Code With Deep <deeppoptani777@gmail.com>
2025-09-08 19:40:16 +0530

    Add files via upload

---
+++ Oracle/Practical Assignments/Sequence.sql
@@ -0,0 +1,16 @@
+CREATE TABLE students(Sno NUMBER (5), Sname VARCHAR2 (30), Scity VARCHAR2 (20));
+
+CREATE SEQUENCE d
+START with 1
+INCREASE by 1
+MAXVALUE 100
+CACHE 20
+CYCLE ORDER;
+
+INSERT INTO students VALUES(d.nextval,'Student 1' ,'India');
+INSERT INTO students VALUES(d.nextval,'Student 2' ,'Las Vegas');
+INSERT INTO students VALUES(d.nextval,'Student 3' ,'Norway');
+INSERT INTO students VALUES(d.nextval,'Student 4' ,'Switzerland');
+INSERT INTO students VALUES(d.nextval,'Student 5' ,'Us');
+
+SELECT * FROM students;
