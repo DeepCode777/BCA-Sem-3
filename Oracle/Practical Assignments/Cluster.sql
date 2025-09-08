a840ac731975013f9a9656d5d8ae6c56d92b1224
Code With Deep <deeppoptani777@gmail.com>
2025-09-08 19:41:47 +0530

    Add files via upload

---
+++ Oracle/Practical Assignments/Cluster.sql
@@ -0,0 +1,20 @@
+CREATE CLUSTER c1(dno NUMBER (5));
+
+CREATE INDEX c2 ON CLUSTER c1;
+
+CREATE TABLE dept(dno NUMBER (5), dname VARCHAR2 (30)) CLUSTER c1(dno);
+
+CREATE TABLE emp(eno NUMBER (5), ename VARCHAR2 (30), dno NUMBER (5)) CLUSTER c1(dno);
+
+INSERT INTO dept(1,'Sales');
+INSERT INTO dept(2,'Management');
+INSERT INTO dept(3,'Account');
+
+INSERT INTO emp(1,'Emp 1',2);
+INSERT INTO emp(2,'Emp 2',3);
+INSERT INTO emp(3,'Emp 3',1);
+INSERT INTO emp(4,'Emp 4',2);
+
+SELECT rowid,dno,dname FROM dept;
+
+SELECT rowid, eno,ename,dno FROM emp;
