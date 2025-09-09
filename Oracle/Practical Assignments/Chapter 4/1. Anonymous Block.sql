0b1579d112586640050dbda4e2eca6b6f3c160a4
Code With Deep <deeppoptani777@gmail.com>
2025-09-09 11:18:31 +0530

    Create 1. Anonymous Block.sql

---
+++ Oracle/Practical Assignments/Chapter 4/1. Anonymous Block.sql
@@ -0,0 +1,14 @@
+DECLARE 
+  a NUMBER(5);
+  b NUMBER (5);
+  c NUMBER (5);
+BEGIN 
+  a := &a;
+  b := &b;
+IF a > b THEN 
+  c := a;
+ELSE 
+  c := b;
+END IF 
+  DBMS_OUTPUT.PUT_LINE('Bigger Number Is : ' ||c);
+END;
