7dcb264b111cef1b6fc0967b3fb5a74bbd034d61
Code With Deep <deeppoptani777@gmail.com>
2025-09-25 09:19:26 +0530

    Create Palindrome Number.sql

---
+++ Oracle/Practical Assignments/Extra/Palindrome Number.sql
@@ -0,0 +1,22 @@
+DECLARE
+  num NUMBER (5);
+  temp NUMBER (5);
+  m NUMBER (5);
+  rem NUMBER (5);
+BEGIN
+
+  num := &num;
+  m := num;
+  WHILE m > 0 LOOP
+    rem := MOD(m,10);
+    temp := (temp * 10) + rem;
+    m := m/10;
+  END LOOP;
+
+  IF num = temp THEN
+    DBMS_OUTPUT.PUT_LINE('Yes, ' || num || ' Is Palindrome Number');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('No, '|| num || ' Is Not Palindrome Number');
+  END IF;
+
+END;
