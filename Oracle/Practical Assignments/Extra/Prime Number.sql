2c35a0ed4522e7c1aa155b55aff44ea10d7672c5
Code With Deep <deeppoptani777@gmail.com>
2025-09-25 08:53:49 +0530

    Create Extra/Prime Number.sql

---
+++ Oracle/Practical Assignments/Extra/Prime Number.sql
@@ -0,0 +1,18 @@
+DECLARE
+    n NUMBER (5);
+    i NUMBER (5);
+    c Number (5);
+BEGIN
+    n := &n;
+  FOR i IN 1..n
+  LOOP
+    IF ((MOD(N,i)) = 0) THEN
+      c := c + 1;
+    END IF;
+  END LOOP;
+  IF (C = 2) THEN
+    DBMS_OUTPUT.PUT_LINE('Number Is Prime');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('Number Not Is Prime');
+  END IF;
+END;
