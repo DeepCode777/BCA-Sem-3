c7c5ab9547d88fd555e7551db0f5664d1ddbaeb2
Code With Deep <deeppoptani777@gmail.com>
2025-09-30 10:07:47 +0530

    Create 5. Large Number.sql

---
+++ Oracle/Practical Assignments/Chapter 4/5. Large Number.sql
@@ -0,0 +1,15 @@
+DECLARE
+  a NUMBER (5);
+  b NUMBER (5);
+  c NUMBER (5);
+BEGIN
+  a := &a;
+  b := &b;
+
+  IF a > b THEN
+    c := a;
+  ELSE
+    c := b;
+  END IF;
+    DBMS_OUTPUT.PUT_LINE('Larger Number Is : '||c);
+END;
