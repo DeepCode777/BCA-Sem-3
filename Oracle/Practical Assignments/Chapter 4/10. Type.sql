a8f05118efe0b5038a148e22547a435f84fd03ae
Code With Deep <deeppoptani777@gmail.com>
2025-09-30 19:02:45 +0530

    Create 10. Type.sql

---
+++ Oracle/Practical Assignments/Chapter 4/10. Type.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employee(eid NUMBER (5) PRIMARY KEY, ename VARCHAR2 (30), salary NUMBER (5));
+
+INSERT INTO employee VALUES (1,'Employee 1',10000);
+INSERT INTO employee VALUES (2,'Employee 2',20000);
+INSERT INTO employee VALUES (3,'Employee 3',50000);
+
+DECLARE
+  en employee.ename%TYPE;
+  es employee.salary%TYPE;
+  n NUMBER (5);
+BEGIN
+  n := &eid;
+  SELECT ename,salary INTO en,es FROM employee WHERE eid = n;
+    DBMS_OUTPUT.PUT_LINE(en);
+    DBMS_OUTPUT.PUT_LINE(es);
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('Try Again');
+END;
