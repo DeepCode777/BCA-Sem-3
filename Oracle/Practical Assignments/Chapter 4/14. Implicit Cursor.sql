c5f03cc02d81c01873f6d2b5819a598b37a2d022
Code With Deep <deeppoptani777@gmail.com>
2025-10-01 15:14:57 +0530

    Create 14. Implicit Cursor.sql

---
+++ Oracle/Practical Assignments/Chapter 4/14. Implicit Cursor.sql
@@ -0,0 +1,17 @@
+CREATE TABLE emp (eid NUMBER (5) PRIMARY KEY, ename VARCHAR2 (30), esalary NUMBER (5));
+
+INSERT INTO emp VALUES (1, 'E1', 10000);
+INSERT INTO emp VALUES (2, 'E2', 20000);
+INSERT INTO emp VALUES (3, 'E3', 30000);
+
+DECLARE
+  ic number(2);
+BEGIN
+  UPDATE emp SET esalary = esalary + 500;
+    IF SQL%NOTFOUND THEN
+      DBMS_OUTPUT.PUT_LINE('No Change');
+    ELSIF SQL%FOUND THEN    
+    ic := SQL%ROWCOUNT;
+      DBMS_OUTPUT.PUT_LINE(ic || 'Rows Changes');
+    END IF;
+END;
