d98ef9b29429d814eafc11f2f43e6c5659db371c
Code With Deep <deeppoptani777@gmail.com>
2025-10-01 15:56:06 +0530

    Update Foreign_Key.sql

--- Oracle/Practical Assignments/Foreign_Key.sql
+++ Oracle/Practical Assignments/Foreign_Key.sql
@@ -1,4 +1,8 @@
-CREATE TABLE library (Bid NUMBER (5) PRIMARY KEY, Bname VARCHAR2 (30), Bprice NUMBER (5));
+CREATE TABLE library (
+  Bid NUMBER (5) PRIMARY KEY,
+  Bname VARCHAR2 (30),
+  Bprice NUMBER (5)
+);
 
 INSERT INTO library VALUES(1,'Oracle','1000');
 INSERT INTO library VALUES(2,'Python','900');
@@ -6,7 +10,14 @@ INSERT INTO library VALUES(3,'CPP','850');
 
 SELECT * FROM library;
 
-CREATE TABLE students (Sid NUMBER (5) PRIMARY KEY, Bid NUMBER (5), Sname VARCHAR2 (30), Sclass VARCHAR2 (5), CONSTRAINT deep FOREIGN KEY (Bid) REFERENCES library (Bid));
+CREATE TABLE students (
+  Sid NUMBER (5) PRIMARY KEY,
+  Bid NUMBER (5),
+  Sname VARCHAR2 (30),
+  Sclass VARCHAR2 (5),
+  CONSTRAINT deep FOREIGN KEY (Bid) 
+  REFERENCES library (Bid)
+);
 
 INSERT INTO students VALUES(1,2,'Std 1','B');
 INSERT INTO students VALUES(2,2,'Std 2','A');
