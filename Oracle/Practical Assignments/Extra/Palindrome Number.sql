907661322cecf05cf39d33d05d662c061ce1c5ed
Code With Deep <deeppoptani777@gmail.com>
2025-10-01 16:06:57 +0530

    Update Palindrome Number.sql

--- Oracle/Practical Assignments/Extra/Palindrome Number.sql
+++ Oracle/Practical Assignments/Extra/Palindrome Number.sql
@@ -4,9 +4,9 @@ DECLARE
   m NUMBER (5);
   rem NUMBER (5);
 BEGIN
-
   num := &num;
   m := num;
+
   WHILE m > 0 LOOP
     rem := MOD(m,10);
     temp := (temp * 10) + rem;
@@ -18,5 +18,4 @@ BEGIN
   ELSE
     DBMS_OUTPUT.PUT_LINE('No, '|| num || ' Is Not Palindrome Number');
   END IF;
-
 END;
