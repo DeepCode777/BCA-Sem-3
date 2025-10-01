415f7c34ca77745b1d4102f844680a006f37e101
Code With Deep <deeppoptani777@gmail.com>
2025-10-01 15:56:39 +0530

    Update Practical_1.sql

--- Oracle/Practical Assignments/Practical_1.sql
+++ Oracle/Practical Assignments/Practical_1.sql
@@ -1,4 +1,8 @@
-CREATE TABLE heros(no NUMBER (5) PRIMARY KEY, name VARCHAR2 (30), city VARCHAR2 (20));
+CREATE TABLE heros(
+  no NUMBER (5) PRIMARY KEY,
+  name VARCHAR2 (30),
+  city VARCHAR2 (20)
+);
 
 INSERT INTO heros VALUES(1,'Lucifer Morningstar','Los Angeles');
 INSERT INTO heros VALUES(2,'Tony Stark','New York');
