ec2a31653bc43b3e0dc76b2f10624a7a799aeef3
Code With Deep <deeppoptani777@gmail.com>
2025-11-23 10:49:20 +0530

    Create Friend Function My.cpp

---
+++ CPP/Chapter 2/My Codes/Friend Function My.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class abc{
+    int a,b;
+    public :
+    abc(){
+        cout << "\n Enter A : ";
+        cin >> a;
+        cout << "\n Enter B : ";
+        cin >> b;
+    }
+    friend void ad(abc l);
+};
+
+void ad(abc l){
+    cout << "\n Sum Of A + B = " << l.a + l.b << endl;
+}
+
+int main() {
+    abc d;
+    ad(d);
+    return 0;
+}
